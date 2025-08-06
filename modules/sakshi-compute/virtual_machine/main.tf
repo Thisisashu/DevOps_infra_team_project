daa70316039d4aab778b01b4b9a25aa1d4272041
Thisisashu <ashuagra481990@gmail.com>
2025-08-06 21:54:01 +0530

    added the codes

---
+++ modules/sakshi-compute/virtual_machine/main.tf
@@ -0,0 +1,27 @@
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "ubuntu-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub") 
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+ source_image_reference {
+  publisher = "Canonical"
+  offer     = "UbuntuServer"
+  sku       = "20_04-lts"
+  version   = "latest"
+}
+
+disable_password_authentication = true
+}
