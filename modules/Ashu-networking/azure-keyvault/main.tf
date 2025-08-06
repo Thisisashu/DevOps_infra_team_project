2d3b7258508a733b0eafeb5a9569fa1b5898867c
Thisisashu <ashuagra481990@gmail.com>
2025-08-06 20:08:23 +0530

    adding the codes

---
+++ modules/Ashu-networking/azure-keyvault/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_key_vault" "my-vault" {
+  name                        = "mykeyvault"
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  enabled_for_disk_encryption = true
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions     = ["Get"]
+    secret_permissions  = ["Get"]
+    storage_permissions = ["Get"]
+  }
+}
